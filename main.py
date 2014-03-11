2b0a446a44056e147ebc2cc1cdd38361834d7d7e
Sam Acuna <samueleishion@gmail.com>
2014-03-11 14:47:22 -0500

    Created utils file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pkg.graphics import * 
+from pkg.utils import * 
 from bio.neuron import * 
 from settings import * 
 
 def main(): 
-	win = GraphWin("Neural Network Graph",WINDOW_X,WINDOW_Y) 
 	n = Neuron(0,.5,1) 
 	n.draw(win,50,50) 
 
---
+++ pkg/utils.py
@@ -0,0 +1,10 @@
+from settings import * 
+from graphics import * 
+
+SENSORIAL 	= 0
+TRANSMITTER = 1 
+TERMINAL 	= 2 
+MOTOR 		= 3 
+
+win = GraphWin("Neural Network Graph",WINDOW_X,WINDOW_Y) 
+
